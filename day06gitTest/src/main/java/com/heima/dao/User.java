7de4f1e02502ee35c4c355bdd459088fc854b5af
sihuilin <782302498@qq.com>
2019-03-28 18:19:27 +0800

    推送

--- day06gitTest/src/main/java/com/heima/dao/User.java
+++ day06gitTest/src/main/java/com/heima/dao/User.java
@@ -4,6 +4,38 @@ public class User {
     private Integer id;
     private String name;
 
-
+    private Integer dsds;
     private String nii;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDsds() {
+        return dsds;
+    }
+
+    public void setDsds(Integer dsds) {
+        this.dsds = dsds;
+    }
+
+    public String getNii() {
+        return nii;
+    }
+
+    public void setNii(String nii) {
+        this.nii = nii;
+    }
 }
